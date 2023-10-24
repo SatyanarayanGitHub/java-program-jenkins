0efb0fbfd3dfe3831e9cf973893817a9c4f3e59b
Satya <satyanarayan.y@gmail.com>
2023-10-24 21:42:41 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("This is simple Java Program");
+        System.out.println("Current Date : " + new Date());
+
+
+    }
+}
